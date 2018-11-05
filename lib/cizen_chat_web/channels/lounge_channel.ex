78a413360e75a923d95d6c2ec59a40ade5329415
Yuki Kodama <endflow.net@gmail.com>
2018-11-06 03:11:27 +0900

    Use Dispatcher.listen/1
    Closes #8

--- lib/cizen_chat_web/channels/lounge_channel.ex
+++ lib/cizen_chat_web/channels/lounge_channel.ex
@@ -1,80 +1,45 @@
-alias Cizen.Effects.{Subscribe, Dispatch, Request, Receive, Start}
-alias Cizen.{Event, Filter}
+alias Cizen.Effects.{Dispatch, Request}
+alias Cizen.{Dispatcher, Event, Filter}
 alias CizenChat.Events.{Transport, Lounge, Room, Avatar}
-
-defmodule CizenChatWeb.Gateway do
-  alias Phoenix.Channel
-  use Cizen.Automaton
-
-  defstruct [:socket, :avatar_id]
-
-  @impl true
-  def spawn(id, %__MODULE__{socket: socket, avatar_id: avatar_id}) do
-    perform id, %Subscribe{
-      event_filter: Filter.new(
-        fn %Event{body: %Transport{dest: dest_id, direction: dir}} ->
-          dest_id == avatar_id and dir == :outgoing
-        end
-      )
-    }
-
-    # FIXME: Advertising should be requested from Cizen, not from the external layer
-    perform id, %Dispatch{
-      body: %Room.Advertise{
-        joiner_id: avatar_id
-      }
-    }
-
-    %{
-      avatar_id: avatar_id,
-      socket: socket
-    }
-  end
-
-  @impl true
-  def yield(id, state) do
-    IO.puts("Gateway[#{state.avatar_id}]")
-    event = perform id, %Receive{}
-    case event.body do
-      %Transport{dest: _dest, direction: _direction, body: body} ->
-        case body do
-          %Room.Message{source: source, dest: _dest, room_id: room_id, text: text} ->
-            IO.puts("Gateway[#{state.avatar_id}] <= Transport(Room.Message): room=#{room_id}")
-            Channel.push state.socket, "room:message", %{source: source, room_id: room_id, body: text}
-          %Room.Setting{source: _source, room_id: room_id, name: name, color: color} ->
-            IO.puts("Gateway[#{state.avatar_id}] <= Transport(Room.Setting): room=#{room_id}")
-            Channel.push state.socket, "room:setting", %{room_id: room_id, name: name, color: color}
-        end
-    end
-    state
-  end
-end
 
 defmodule CizenChatWeb.LoungeChannel do
   use Phoenix.Channel
   use Cizen.Effectful
 
   def join("lounge:hello", _message, socket) do
-    %{body: %{avatar_id: id, avatar_name: name}} = handle fn id ->
+    %{body: %{avatar_id: avatar_id, avatar_name: name}} = handle fn id ->
       perform id, %Request{body: %Lounge.Join{}}
     end
 
-    send(self(), {:after_join, id})
+    Dispatcher.listen(Filter.new(
+      fn %Event{body: %Transport{dest: dest_id, direction: dir}} ->
+        dest_id == avatar_id and dir == :outgoing
+      end
+    ))
 
-    {:ok, %{id: id, name: name}, socket}
+    # FIXME: Request advertising to rooms
+    handle fn id ->
+      perform id, %Dispatch{
+        body: %Room.Advertise{joiner_id: avatar_id}
+      }
+    end
+
+    {:ok, %{id: avatar_id, name: name}, socket}
   end
 
   def join("lounge:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
   end
 
-  def handle_info({:after_join, avatar_id}, socket) do
-    handle fn id ->
-      perform id, %Start{
-        saga: %CizenChatWeb.Gateway{socket: socket, avatar_id: avatar_id}
-      }
-    end
+  def handle_info(%Event{body: %Transport{dest: dest, body: %Room.Message{source: source, dest: _dest, room_id: room_id, text: text}}}, socket) do
+    IO.puts("LoungeChannel[#{dest}] <= Transport(Room.Message): room=#{room_id}")
+    push socket, "room:message", %{source: source, room_id: room_id, body: text}
+    {:noreply, socket}
+  end
 
+  def handle_info(%Event{body: %Transport{dest: dest, body: %Room.Setting{source: _source, room_id: room_id, name: name, color: color}}}, socket) do
+    IO.puts("LoungeChannel[#{dest}] <= Transport(Room.Setting): room=#{room_id}")
+    push socket, "room:setting", %{room_id: room_id, name: name, color: color}
     {:noreply, socket}
   end
 
